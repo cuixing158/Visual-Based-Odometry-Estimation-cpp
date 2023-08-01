a91e7710a72dc9a43ed71e441a87c98194f94787
cuixing158 <1229261804@qq.com>
2023-08-01 10:29:13 +0800

    fix: undefined library to be fixed

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 
 #include "c_cpp_utils/path.h"
 #include "src/HDMapping.h"
+#include "estimateAffineRigid2D.h"
 
 int main(int, char **) {
     std::string imagePath = "opt_disk2/rd22946/AllDataAndModels/from_tongwenchao/116_new_undistort/116";  // opt_disk2/rd22946/AllDataAndModels/from_tongwenchao/116_new_undistort/116";  //"/opt_disk2/rd22946/AllDataAndModels/from_tongwenchao/map_R_new_undistort/map_R";
@@ -25,6 +26,25 @@ int main(int, char **) {
     //           [](std::string p1, std::string p2) { return atoi(filesystem::path(p1).filenameNoExt().substr(6).c_str()) < atoi(filesystem::path(p2).filenameNoExt().substr(6).c_str()); });
     std::sort(imagePaths.begin(), imagePaths.end(),
               [](std::string p1, std::string p2) { return atoi(filesystem::path(p1).filenameNoExt().c_str()) < atoi(filesystem::path(p2).filenameNoExt().c_str()); });
+
+    int status = 0;
+    coder::array<double, 2U> pts1_tmp, pts2_tmp;
+    coder::array<boolean_T, 2U> inlierIndex;
+    double tform2x3_[6];
+
+    pts1_tmp.set_size(5, 2);
+    pts2_tmp.set_size(5, 2);
+    for (size_t i = 0; i < 5; i++) {
+        pts1_tmp[i] = 1;
+        pts1_tmp[i + 5] = 2;
+
+        pts2_tmp[i] = 1;
+        pts2_tmp[i + 5] = 2;
+    }
+    estimateAffineRigid2D::estimateAffineRigid2D(pts1_tmp, pts2_tmp, tform2x3_, inlierIndex, &status);
+    cv::Mat inliers = cv::Mat(5, 1, CV_8U, inlierIndex.data()).clone();
+    cv::Mat tform2x3 = (cv::Mat_<double>(2, 3) << tform2x3_[0], tform2x3_[2], tform2x3_[4], tform2x3_[1], tform2x3_[3], tform2x3_[5]);
+    std::cout << "tform2x3:" << tform2x3 << std::endl;
 
     buildMapping::HDMapping obj;
     double cumTime = 0.0;
